96f415a0f91305c1ed8704c69c018de16548b3d0
Daniil Kirsanov <dankirsdot@gmail.com>
2021-05-07 03:41:31 +0300

    fixed run.sh path

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 ./src/main.py -n yolo-v3-tiny-tf -f /home/openvino/public/ -p FP32 -v /home/openvino/simple_tracker/video.mp4
+python3 /home/openvino/simple_tracker/src/main.py -n yolo-v3-tiny-tf -f /home/openvino/public/ -p FP32 -v /home/openvino/simple_tracker/video.mp4
